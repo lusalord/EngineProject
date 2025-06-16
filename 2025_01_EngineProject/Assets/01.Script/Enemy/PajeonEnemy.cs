f8794e8b5d58b8e1550e945ec3b3144741d23e70
lusalord <lusalord63@gmail.com>
2025-06-17 00:50:35 +0900

    feat : Enemy

--- 2025_01_EngineProject/Assets/01.Script/Enemy/PajeonEnemy.cs
+++ 2025_01_EngineProject/Assets/01.Script/Enemy/PajeonEnemy.cs
@@ -1,11 +1,13 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace _01.Script.Enemy
 {
     public class PajeonEnemy : MonoBehaviour
     {
         private HealthSystem _healthSystem;
+        [FormerlySerializedAs("_itemPrefab")] [SerializeField] private GameObject itemPrefab;
         
         
         private void Awake()
@@ -17,6 +19,7 @@ namespace _01.Script.Enemy
         {
             if (_healthSystem.isDead)
             {
+                Instantiate(itemPrefab, transform.position, Quaternion.identity);
                 Destroy(gameObject);
             }
         }
--- 2025_01_EngineProject/Assets/01.Script/Item/Pa.cs
+++ 2025_01_EngineProject/Assets/01.Script/Item/Pa.cs
@@ -1,15 +1,9 @@
+using _01.Script.Interface;
 using UnityEngine;
 
-public class Pa : MonoBehaviour
+public class Pa : MonoBehaviour, IItem
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    public void Use(GameObject target)
     {
         
     }
